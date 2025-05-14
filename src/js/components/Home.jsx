6ac641885f56b8a94c8593576ba0a83853414812
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-05-14 11:28:07 +0000

    estructura de todo list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [input,setInput] = useState("");
 	return (
+		<>
 		<div className="text-center">
-            
+        <div className="todo-list">
+			<input value={input} type="text" onChange={() => {setInput(e.target.value)}}/>
+			<button className="btn btn-light">Agregar tarea</button>
+			<ul>
+				<li></li>
+			</ul>
+		</div>    
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
+
+		</>
 	);
 };
 
